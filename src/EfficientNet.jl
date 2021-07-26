42d3c88ec264d745d15b0a790d8013e91455fec4
Anton Smirnov <anton.smn@protonmail.com>
2021-07-26 16:11:33 +0300

    Remove local stuff

--- src/EfficientNet.jl
+++ src/EfficientNet.jl
@@ -11,26 +11,4 @@ include("utils.jl")
 include("model.jl")
 include("load_utils.jl")
 
-function main()
-    device = gpu
-    model = EffNet("efficientnet-b0")
-    # model = from_pretrained("efficientnet-b0")
-    model = model |> testmode! |> device
-    @info "Model loaded on device $device."
-    @info "Stages channels: $(stages_channels(model))"
-    @info "Stages: $(get_stages(model))"
-
-    x = randn(Float32, 256, 256, 3, 1) |> device
-
-    features = model(x, Val(:stages_map))
-    features = model(x, Val(:stages_map))
-    @info "Features: $(length(features))"
-    for f in features
-        @info "Feature size: $(size(f))"
-    end
-
-    x |> model
-end
-main()
-
 end
